1ec32e70731af95692c0324e4b068175b4c84f29
peterldow <LeagueAilime@gmail.com>
2024-09-27 14:40:32 +0800

    9-27

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@
         <span class="name">
           {{ siteName }}
         </span>
-        <span class="tip"> 加载中 </span>
+        <span class="tip"> 加载中..如果长时间无法加载请刷新页面 </span>
       </div>
     </div>
     <div class="loader-section section-left" />
