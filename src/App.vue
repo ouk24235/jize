8671297b1ef9d77f7949cb3ed55b3e272e203f35
peterldow <LeagueAilime@gmail.com>
2024-09-30 04:07:37 +0800

    9-30-2

--- src/App.vue
+++ src/App.vue
@@ -118,7 +118,7 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "Cocoaの主页";
+  const title1 = "Assiwの主页";
   const title2 = `
  _____ __  __  _______     ____     __
 |_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -41,7 +41,7 @@ const openMusicShow = ref(false);
 // 一言数据
 const hitokotoData = reactive({
   text: "这里应该显示一句话",
-  from: "Cocoa",
+  from: "Assiw",
 });
 
 // 获取一言数据
@@ -59,7 +59,7 @@ const getHitokotoData = async () => {
       }),
     });
     hitokotoData.text = "这里应该显示一句话";
-    hitokotoData.from = "Cocoa";
+    hitokotoData.from = "Assiw";
   }
 };
 
